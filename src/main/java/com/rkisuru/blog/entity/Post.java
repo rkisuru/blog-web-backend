ca8e1c0ec63b7d36c1be37da1fb382d2d7de17aa
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-06-29 12:31:46 +0530

    Added model

---
+++ src/main/java/com/rkisuru/blog/entity/Post.java
@@ -0,0 +1,32 @@
+package com.rkisuru.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(length = 10000)
+    private String content;
+
+    private String postedBy;
+
+    private String img;
+
+    private Date date;
+
+    private int likeCount;
+    private int viewCount;
+
+    private List<String> tags;
+
+}
