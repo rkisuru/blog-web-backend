b2d78456990f7931687d6f6635745c1ac093e51b
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-06-29 21:07:52 +0530

    Created post service

--- src/main/java/com/rkisuru/blog/service/PostServiceImpl.java
+++ src/main/java/com/rkisuru/blog/service/PostServiceImpl.java
@@ -1,8 +1,24 @@
 package com.rkisuru.blog.service;
 
+import com.rkisuru.blog.entity.Post;
+import com.rkisuru.blog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class PostServiceImpl implements PostService{
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post SavePost(Post post){
+        post.setLikeCount(0);
+        post.setViewCount(0);
+        post.setDate(new Date());
+
+        return postRepository.save(post);
+    }
 
 }
