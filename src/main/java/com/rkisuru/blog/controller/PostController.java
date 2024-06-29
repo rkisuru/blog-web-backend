5f43311f71961db3002e57f696eb2678329c29b7
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-06-29 22:21:12 +0530

    PostService dependency injection

--- src/main/java/com/rkisuru/blog/controller/PostController.java
+++ src/main/java/com/rkisuru/blog/controller/PostController.java
@@ -1,5 +1,30 @@
 package com.rkisuru.blog.controller;
 
+import com.rkisuru.blog.entity.Post;
+import com.rkisuru.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<?> createPost(@RequestBody Post post){
+        try {
+            Post createdPost = postService.SavePost(post);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 }
--- src/main/java/com/rkisuru/blog/service/PostService.java
+++ src/main/java/com/rkisuru/blog/service/PostService.java
@@ -1,4 +1,7 @@
 package com.rkisuru.blog.service;
 
+import com.rkisuru.blog.entity.Post;
+
 public interface PostService {
+    Post SavePost(Post post);
 }
