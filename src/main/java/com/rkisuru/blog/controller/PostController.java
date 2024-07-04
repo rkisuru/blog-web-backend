19c86a695508c24f874d3c52901f1bfa711d2927
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-04 19:06:41 +0530

    feat: Implement likePost method

--- src/main/java/com/rkisuru/blog/controller/PostController.java
+++ src/main/java/com/rkisuru/blog/controller/PostController.java
@@ -2,6 +2,7 @@ package com.rkisuru.blog.controller;
 
 import com.rkisuru.blog.entity.Post;
 import com.rkisuru.blog.service.PostService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +33,26 @@ public class PostController {
             return ResponseEntity.status(HttpStatus.OK).body(postService.getAllPosts());
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getPostById(@PathVariable Long postId){
+        try {
+            Post post = postService.getPostById(postId);
+            return ResponseEntity.ok(post);
+        }
+        catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+    @PutMapping("/{postId}/like")
+    public ResponseEntity<?> likePost(@PathVariable Long postId){
+
+        try {
+            postService.likePost(postId);
+            return ResponseEntity.ok(new String[]{"Post liked successfully!"});
+        } catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
--- src/main/java/com/rkisuru/blog/service/PostService.java
+++ src/main/java/com/rkisuru/blog/service/PostService.java
@@ -8,6 +8,7 @@ public interface PostService {
     Post savePost(Post post);
     List<Post> getAllPosts();
     Post getPostById(Long postId);
+    void likePost(Long postId);
 }
 
 
