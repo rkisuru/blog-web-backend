8c25cb601e7c149604dfb775486f10111243a048
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-28 12:34:56 +0530

    add search posts by title query

--- src/main/java/com/rkisuru/blog/repository/PostRepository.java
+++ src/main/java/com/rkisuru/blog/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.rkisuru.blog.repository;
 
 import com.rkisuru.blog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findAllByTitle(String name);
+    @Query("SELECT posts FROM Post posts WHERE posts.title LIKE %:title%")
+    List<Post> findAllByTitle(@Param("title") String title);
+
     List<Post> findByTags(List<String> tags);
 }
