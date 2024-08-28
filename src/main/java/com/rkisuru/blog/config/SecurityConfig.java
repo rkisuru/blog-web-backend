ae7b9d8c7f198e560fe2f44bb351829033715782
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-28 12:30:02 +0530

    feat: implement Swagger UI

--- src/main/java/com/rkisuru/blog/config/SecurityConfig.java
+++ src/main/java/com/rkisuru/blog/config/SecurityConfig.java
@@ -22,7 +22,16 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request-> request.requestMatchers(
-                        "/x"
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
                 ).permitAll()
                         .anyRequest()
                         .authenticated()
