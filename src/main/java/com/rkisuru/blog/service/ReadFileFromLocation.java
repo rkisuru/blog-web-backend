512ac212d7b7b3db646f3c2ebf42feba66cfeecd
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-01 22:15:35 +0530

    implement ReadFileFromLocation

---
+++ src/main/java/com/rkisuru/blog/service/ReadFileFromLocation.java
@@ -0,0 +1,27 @@
+package com.rkisuru.blog.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
+public class ReadFileFromLocation {
+
+    public static byte[] readFile(String fileUrl) {
+
+        if (StringUtils.isBlank(fileUrl)) {
+            return null;
+        }
+        try {
+            Path filePath = new File(fileUrl).toPath();
+            return Files.readAllBytes(filePath);
+        } catch (IOException e) {
+            log.warn("No file found at {}", fileUrl);
+        }
+        return null;
+    }
+}
