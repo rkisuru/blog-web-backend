44ba3e13a11ce0ba1767f96473b16df98ecf9206
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-24 12:31:12 +0530

    implement PostType enum

---
+++ src/main/java/com/rkisuru/blog/type/PostType.java
@@ -0,0 +1,14 @@
+package com.rkisuru.blog.type;
+
+public enum PostType {
+
+    ANY,
+    LIFESTYLE,
+    TECHNOLOGY,
+    BUSINESS,
+    EDUCATION,
+    ENTERTAINMENT,
+    SPORTS,
+    NEWS,
+    PERSONAL
+}
