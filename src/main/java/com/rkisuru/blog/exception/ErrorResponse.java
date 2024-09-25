af865e8edfe7e8e4b8a2f4bcf0f906f231ec006e
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-25 19:12:38 +0530

    implement error response

---
+++ src/main/java/com/rkisuru/blog/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.rkisuru.blog.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ErrorResponse {
+
+    private String message;
+    private int code;
+}
