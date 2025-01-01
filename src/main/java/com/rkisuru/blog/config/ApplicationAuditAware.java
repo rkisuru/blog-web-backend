b41ac394cb25800bca9092310db61bd2af064b54
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-01-01 22:37:44 +0530

    refactor: change principal to Oauth2User

--- src/main/java/com/rkisuru/blog/config/ApplicationAuditAware.java
+++ src/main/java/com/rkisuru/blog/config/ApplicationAuditAware.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Optional;
 
@@ -16,6 +17,11 @@ public class ApplicationAuditAware implements AuditorAware<String> {
                 !authentication.isAuthenticated() ||
                 authentication instanceof AnonymousAuthenticationToken){
             return Optional.empty();
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof OAuth2User) {
+            String username = ((OAuth2User) principal).getAttribute("sub");
+            return Optional.ofNullable(username);
         }
         return Optional.ofNullable(authentication.getName());
     }
--- src/main/java/com/rkisuru/blog/service/CommentService.java
+++ src/main/java/com/rkisuru/blog/service/CommentService.java
@@ -10,7 +10,8 @@ import com.rkisuru.blog.request.CommentEditRequest;
 import com.rkisuru.blog.request.CommentRequest;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,12 +41,12 @@ public class CommentService {
         throw new Exception("Comment cannot be empty!");
     }
 
-    public Comment editComment(Long commentId, CommentEditRequest request, Authentication connectedUser) {
+    public Comment editComment(Long commentId, CommentEditRequest request, @AuthenticationPrincipal OAuth2User user) {
 
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new EntityNotFoundException("Comment not found"));
 
-            if (comment.getPostedBy().equals(connectedUser.getName())) {
+            if (comment.getPostedBy().equals(user.getAttribute("sub"))) {
 
                 if (!request.content().isBlank()) {
                     comment.setContent(request.content());
@@ -56,12 +57,12 @@ public class CommentService {
             throw new OperationNotPermittedException("You do not have permission to edit this comment");
     }
 
-    public void deleteComment(Long commentId, Authentication connectedUser) {
+    public void deleteComment(Long commentId, @AuthenticationPrincipal OAuth2User user) {
 
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new EntityNotFoundException("Comment not found"));
 
-            if (comment.getPostedBy().equals(connectedUser.getName())) {
+            if (comment.getPostedBy().equals(user.getAttribute("sub"))) {
                 commentRepository.delete(comment);
             }
             throw new OperationNotPermittedException("You do not have permission to delete this comment");
