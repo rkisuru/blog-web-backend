60aa201f6c775c44f30f7de22fa053302871e9f0
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-01-01 22:37:06 +0530

    implement OAuth2 authentication

--- src/main/java/com/rkisuru/blog/config/SecurityConfig.java
+++ src/main/java/com/rkisuru/blog/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.rkisuru.blog.config;
 
-import com.rkisuru.blog.keycloak.KeycloakJwtAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,27 +19,32 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
-        http
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(request-> request.requestMatchers(
-                                        "/v2/api-docs",
-                                        "/v3/api-docs",
-                                        "/v3/api-docs/**",
-                                        "/swagger-resources",
-                                        "/swagger-resources/**",
-                                        "/configuration/ui",
-                                        "/configuration/security",
-                                        "/swagger-ui/**",
-                                        "/webjars/**",
-                                        "/swagger-ui.html"
-                ).permitAll()
-                        .anyRequest()
-                        .authenticated()
-                )
-                .oauth2ResourceServer(auth-> auth.jwt(token-> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
-        return http.build();
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authorize->authorize.anyRequest().authenticated())
+                .oauth2Login(oauth2->oauth2
+                        .defaultSuccessUrl("http://localhost:5173", true)
+                        .failureUrl("/login")
+                        .loginPage("http://localhost:5173/login"))
+                .logout(logout->logout
+                        .logoutSuccessUrl("http://localhost:5173")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .clearAuthentication(true))
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
