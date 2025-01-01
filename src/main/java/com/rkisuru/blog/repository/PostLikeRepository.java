c8fba517bcb2717d2e6c4924eab039022905bb08
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-01-01 22:45:34 +0530

    implement methods

--- src/main/java/com/rkisuru/blog/repository/PostLikeRepository.java
+++ src/main/java/com/rkisuru/blog/repository/PostLikeRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Integer> {
 
     @Query("SELECT postlike FROM PostLike postlike WHERE postlike.likedBy = :likedBy AND postlike.post.id = :postId")
     Optional<PostLike> findPostLikeById(@Param("likedBy") String likedBy, @Param("postId") Long postId);
+
+    public List<PostLike> findPostLikeByPostId(Long postId);
 }
